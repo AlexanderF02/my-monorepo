8353813d73147850fd5cca56cde65122bff9af8c
AlexanderF02 <alexander.fallstrom5@gmail.com>
2025-10-23 14:24:02 +0200

    Slutversion

---
+++ apps/mobile/app/(tabs)/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text, SafeAreaView } from "react-native";
+
+export default function Index() {
+  return (
+    <SafeAreaView>
+      <View style={{ padding: 20 }}>
+        <Text style={{ fontWeight: "bold" }}>Hello Expo Router</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+
